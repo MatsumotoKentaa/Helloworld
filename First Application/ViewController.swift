7bcc9149c7b73deba7558f9c4972aa83a25e4ddc
松本健汰 <kenta@matsumotokennomacbook.local>
2018-09-21 16:29:58 +0900

    Initial Commit

---
+++ First Application/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  First Application
+//
+//  Created by 松本健汰 on 2018/09/21.
+//  Copyright © 2018年 松本健汰. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
